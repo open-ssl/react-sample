d0674145d5a1e9534f42e311d9dfe55c76b42592
open-ssl <lukyanov.stas@mail.ru>
2022-03-19 17:02:42 +0200

    add memo example

--- memo-example/memo.js
+++ memo-example/memo.js
@@ -3,3 +3,49 @@
 
 // Функция memo возвращает оптимизированный компонент, который будет вызываться только в том случае,
 // если хотя бы один проп этого компонента изменился.
+
+import React, { useState, memo } from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+
+const Title = props => {
+  console.log(`Компонент со строкой: ${props.text} вызвался.`)
+
+  return (
+    <h1>
+      {props.text}
+    </h1>
+  )
+}
+
+
+const MemoizedComponent = memo(Title)
+
+const Wrapper = () => {
+  const [isReversed, reverse] = useState(false)
+  const text = "Нажмите сюда"
+
+  const onClickHandle = () => {
+    reverse(previousIsReversed => !previousIsReversed)
+  }
+
+  return (
+    <div>
+      <h2 onClick={onClickHandle}>
+        {
+          isReversed
+            ? text.split("").reverse().join("")
+            : text
+        }
+      </h2>
+      <MemoizedComponent text="Строка #1"/>
+      <MemoizedComponent text="Строка #2"/>
+      <Title text="Строка #3"/>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <Wrapper/>,
+  document.getElementById('root')
+);
